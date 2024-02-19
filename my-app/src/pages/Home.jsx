2a44b4bb33dd2385ab8221e72674a1932ea18b2c
Dan <danretegan@yahoo.com>
2024-02-19 13:25:17 +0200

    Update Home.jsx

--- my-app/src/pages/Home.jsx
+++ my-app/src/pages/Home.jsx
@@ -1,3 +1,13 @@
 export function Home() {
-  return <div>Home page...</div>;
+  return (
+    <main>
+      <h1>Welcome</h1>
+      <img src="https://via.placeholder.com/960x240" alt="" />
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iusto,
+        laboriosam placeat incidunt rem illum animi nemo quibusdam quia
+        voluptatum voluptate.
+      </p>
+    </main>
+  );
 }
