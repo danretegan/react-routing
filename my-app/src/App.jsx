23139a6f4bc5dd894935939d5ae42d9eec633734
Dan <danretegan@yahoo.com>
2024-02-19 12:19:32 +0200

    add Link

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Link, Route, Routes } from "react-router-dom";
 import "./App.css";
 import { Home } from "./pages/Home";
 import { About } from "./pages/About";
@@ -8,7 +8,11 @@ import { NotFoundPage } from "./pages/NotFoundPage";
 function App() {
   return (
     <>
-      <nav></nav>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/products">Products</Link>
+      </nav>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
