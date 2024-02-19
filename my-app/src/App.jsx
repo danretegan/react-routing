685a8020e723cce957285228d02a953c5f55f306
Dan <danretegan@yahoo.com>
2024-02-19 12:11:24 +0200

    add Routes /Route

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,7 +1,22 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import { Home } from "./pages/Home";
+import { About } from "./pages/About";
+import { Products } from "./pages/Products";
+import { NotFoundPage } from "./pages/NotFoundPage";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <nav></nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
